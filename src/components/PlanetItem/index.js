6459d762ebd7e513f285810ed93df4a026da92bc
Manjushgurajada <manjusha.gurajada@gmail.com>
2023-12-07 03:03:45 +0000

    planet

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+  return (
+    <div className="planet-container">
+      <img src={imageUrl} alt={name} className="img" />
+      <h1 className="planet-name">{name}</h1>
+      <p className="planet-description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
